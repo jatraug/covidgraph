85d11e6e42baa1cea670c62994daa137b1ec4413
jim traugott <jtraug@gmail.com>
2020-04-13 16:33:58 -0700

    Changed tools dir to pytools

--- nyt/cases.py
+++ nyt/cases.py
@@ -9,7 +9,7 @@ import sklearn
 from sklearn.cluster import KMeans
 
 import sys
-sys.path.append('/Users/jimt/work/python/tools')
+sys.path.append('/Users/jimt/work/python/pytools')
 import avg as avg
 
 ##from: https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv
