dae4a8825af3c944209ad019cf7746155efcce5f
jim traugott <jtraug@gmail.com>
2020-06-10 16:01:51 -0700

    add get counties

--- nyt/getlatest.sh
+++ nyt/getlatest.sh
@@ -1,5 +1,7 @@
-curl -o datasets/us-states.csv https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv
+-*-sh-*-
+
+    curl -o datasets/us-states.csv https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv
 
 curl -o datasets/us-counties.csv https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv
 
-./getl
+
