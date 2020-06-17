6b9cf0152db28af290da7838d90e8cf79f25c0b9
jim traugott <jtraug@gmail.com>
2020-06-17 11:50:17 -0700

    Initial checkin

---
+++ nyt/wacounties.py
@@ -0,0 +1,14 @@
+## List latest for each county in Washington
+
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+import sklearn
+from sklearn.cluster import KMeans
+
+import sys
+sys.path.append('/Users/jimt/work/python/pytools')
+import avg as avg
+from options import Options
