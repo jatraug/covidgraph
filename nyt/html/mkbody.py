666d0c6b7536fdb4365b8123e1e54fe535b44178
jim traugott <jtraug@gmail.com>
2021-05-10 12:42:12 -0700

    make html

--- nyt/html/mkbody.py
+++ nyt/html/mkbody.py
@@ -2,19 +2,39 @@
 
 import os, sys
 from stat import *
+import re
 
 pathname = '/Users/jimt/work/covid/nyt/images'
 
+def makeCaption(jname):
+    ''' make a captin from the jpeg name
+
+    '''
+    caption = ''
+    
+    goaway = re.compile(r'_2\d{3}-\d{2}-\d{2}.*')
+    caption = re.sub(goaway, '', jname)
+    return caption
+    
 def mkHtmlWithFiles(pathname, smclass="", lgclass=""):
-    for f in os.listdir(pathname):
+    lcount = 0
+    filearr = []
+    for file in os.listdir(pathname):
+        filearr.append(file)
+    filearr.sort()
+    for f in filearr:
         fname = os.path.join(pathname, f)
         ourstat = os.stat(fname)
         mode = ourstat.st_mode
-        if S_ISREG(mode):
+        if S_ISREG(mode) and f != '.DS_Store':
             print(f'<a img class=\"{lgclass}\" href=\"{fname}\">\n\
-            <img class=\"{smclass}\"src=\"{fname}\">\n\
-            </a>')
-
+            <img class=\"{smclass}\"src=\"{fname}\">\n', end='')
+            
+            print(makeCaption(f), end='') 
+            print('</a>', end='')
+            lcount +=1
+            if lcount %2 == 0:
+                print('<p>')
             ##print(f'<img class=\"{xclass}\" src=\"{fname}\">')
 
 
