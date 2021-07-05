26bc896158079b0b46302f5be37b45cd933739c0
jim traugott <jtraug@gmail.com>
2021-07-05 15:12:02 -0700

    Removed date and county from opts

--- nyt/states.py
+++ nyt/states.py
@@ -18,22 +18,21 @@ class ourOpts(Options):
     def __init__(self, argv, exename):
         super().__init__(argv, exename)
         self.state = 'NoState'
-        self.county = 'NoCounty'
         self.doplot = True # Plot the graph
         self.exename = exename
-        self.date = False
+
         try:
-            opts, args = getopt.getopt(argv, "hs:c:nd:v")
+            opts, args = getopt.getopt(argv, "hs:nv")
         except getopt.GetoptError:
             self.printHelpAndExit()
-#            print(self.exename, ' [-n  (noplot)] [-s State] [-c County]')
+
         count = 0
         for opt, arg in opts:
             count += 1
-            #print(count, opt, arg)
+
             if opt == '-h':
                 self.printHelpAndExit()
-#                print(self.exename, ' [-n  (noplot)] [-s State] [-c County] -v [-d date (2021-03-30)')
+
             elif opt == '-v':
                 print(f'Version {self.getVersion()}')
                 sys.exit()
@@ -41,15 +40,10 @@ class ourOpts(Options):
                 self.doplot = False
             elif opt in ("-s"):
                 self.state = arg
-            elif opt in ('-c'):
-                self.county = arg
-            elif opt in ('-d'):
-                self.date = arg
-                dtfmt = re.compile('\d{4}-\d{2}-\d{2}')
-                matchobj = re.search(dtfmt, self.date)
-                if not matchobj:
-                    self.printHelpAndExit()
-                    sys.exit()
+
+                def printHelpAndExit(self):
+        print(f'usage: {self.exename} [-n  (noplot)] [-s State] -v)')
+        sys.exit('Exiting')
 
 
 
