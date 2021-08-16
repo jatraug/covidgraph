67afee2635d6e05f9d53cdf67f0003f6afe9118a
jim traugott <jtraug@gmail.com>
2021-08-15 20:45:28 -0700

    snapshot - works!

--- nyt/timetools.py
+++ nyt/timetools.py
@@ -1,22 +1,31 @@
 from datetime import datetime, timedelta
 import re
 
-def early_date(beforetime):
-    ini_time_for_now = datetime.now()
-    new_final_time = ini_time_for_now + timedelta(days = beforetime)
+def early_date(endtime, daysbefore):
+    """
+starting with the end date(endtime), calculate and retun the start date daysbefore days before(pant, pant!)
+    input: 
+       -endtime - the end date of the series that we're finding the start date for.
+       -daysbefore in days ( negative days to find the start date
+      from the end date
+    """
+    new_final_time = endtime + timedelta(days = daysbefore)
     rx = re.compile('\d{4}-\d{2}-\d{2}')
-    matchobj = re.search(rx, wholetime)
+    matchobj = re.search(rx, str(new_final_time))
 
     if (matchobj):
         print(matchobj[0])
-   ## print ("new_final_time", str(new_final_time))
+    return matchobj[0]
+
 
 
 
 
 
 def testit():
-    early_date(int(-84))
+    edate = early_date(datetime.fromisoformat('2021-09-24'), int(-84))
+    assert edate == '2021-07-02'
+    
 
 
 if __name__ == '__main__':
