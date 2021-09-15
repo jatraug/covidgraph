a4889dd30a7acb6728e5076eeb0ca615fa860b68
jim traugott <jtraug@gmail.com>
2021-09-15 14:56:58 -0700

    Initial checkin

---
+++ nyt/facts.py
@@ -0,0 +1,19 @@
+"""
+Stuff to know,  like where things are
+"""
+
+
+
+
+NYT = "/Users/jimt/work/covid/nyt"
+HTML = NYT + "/html"
+IMAGES = NYT + "/images"
+DSETS = NYT + "/datasets"
+
+
+
+def main():
+    print(IMAGES)
+
+if __name__ == "__main__":
+    main()
