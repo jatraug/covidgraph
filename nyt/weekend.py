e30a599ec92ca62dcf997ac2934c5550bb1e632b
jim traugott <jtraug@gmail.com>
2021-09-16 15:36:01 -0700

    Changed to class

--- nyt/weekend.py
+++ nyt/weekend.py
@@ -8,29 +8,39 @@ import os
 import sys
 import facts
 lazyStates = ["Washington", "Oregon", "South Carolina"]
+empty = []
+
+
+
 ##  os.remove("file.jpg")
 
+class RemoveFiles:
+    def __init__(self, lazyStates):
+        self.removeFiles = []
+        self.empty = []
+        self.lazyStates = lazyStates
 
+    def doOrDontRemove(self,  pathname):
 
-
-def doOrDontRemove(pathname):
-
-    for f in os.listdir(pathname):
-        dontRemove = False
-        fname = os.path.join(pathname, f)
-
-        for s in lazyStates:
-            if s in f:
-                dontRemove = True
-                break        
-        if dontRemove == True:
-            print(f"keep {f}")
-        else:
-            print(f"remove {f}")
+        for f in os.listdir(pathname):
+            keepfile = False
+            fname = os.path.join(pathname, f)
+                
+            for s in self.lazyStates:
+                if s in f:
+                    keepfile = True
+                    break        
+            if keepfile == True:
+                print(f"keep {f}")
+            else:
+                print(f"remove {f}")
+                self.removeFiles.append(f)
 
 
 
 def main():
-    doOrDontRemove(facts.DBIMG)
+    rf = RemoveFiles(lazyStates)
+    rf.doOrDontRemove(facts.DBIMG)
+    
 if __name__ == "__main__":
     main()
