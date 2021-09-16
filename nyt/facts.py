71f534ac6dfc5f78339f7e9414f781171adf904d
jim traugott <jtraug@gmail.com>
2021-09-16 15:48:35 -0700

    Added paths; added lazyStates

--- nyt/facts.py
+++ nyt/facts.py
@@ -6,12 +6,23 @@ Stuff to know,  like where things are
 
 
 NYT = "/Users/jimt/work/covid/nyt"
+DBOX = "/Users/jimt/Dropbox/work/covid"
+AWSHOME = "/home/bitnami"
+
 HTML = NYT + "/html"
 IMAGES = NYT + "/images"
 DSETS = NYT + "/datasets"
 WEB = NYT + "/web"
+DBIMG = DBOX + "/html/images"
+AWSIMG = AWSHOME + "/htdocs/apps/covid/images"
+
+## States that are too lazy to report on weekends This execise is all about
+## keeping the generated files around if it's a weekend.
+lazyStates = ["Washington", "Oregon", "South Carolina"]
 
 
+
+#### Test below:
 def main():
     print(IMAGES)
 
