064237454abc25bd8927af5df106ccc33aa11410
jim traugott <jtraug@gmail.com>
2021-09-18 19:14:09 -0700

    Snapshot. Flehed some funcs, added some ohers. Ready to Go!

--- nyt/weekend.py
+++ nyt/weekend.py
@@ -19,8 +19,23 @@ class RemoveFiles:
         self.removeFiles = []
         self.empty = []
         self.lazyStates = lazyStates
+        if self.is_weekend():
+            self.doOrDontRemove = self.remove_is_weekend
+        else:
+            self.doOrDontRemove = self.remove_not_weekend
+
+    def is_weekend(self):
+        return tt.is_weekend()
+
+    def remove_not_weekend(self, pathname):
+        for f in os.listdir(pathname):
+            self.removeFiles.append(f)
+        self.prepareAllFileRemoval()
 
     def doOrDontRemove(self,  pathname):
+        pass
+    
+    def remove_is_weekend(self,  pathname):
 
         for f in os.listdir(pathname):
             keepfile = False
@@ -39,27 +54,44 @@ class RemoveFiles:
         self.prepareAllFileRemoval()
 
     def writeNativeRemoveFiles(self):
-        with  open("NativeRemove.txt", "w") as fs:
+        with  open("nativeRemove.txt", "w") as fs:
+            fs.write("import os\n")
             for file in self.removeFiles:
                 pfile = os.path.join(facts.IMAGES, file)
                 fs.write(f"os.remove({pfile})\n")
         fs.close()
 
     def  writeDropboxRemoveFiles(self):
-        pass
+        with  open("dboxRemove.txt", "w") as fs:
+            fs.write("import os\n")
+            for file in self.removeFiles:
+                pfile = os.path.join(facts.DBIMG, file)
+                fs.write(f"os.remove({pfile})\n")
+        fs.close()
+
+
+        
 
     def writeBitnamiRemoveFiles(self):
-        pass
+        with  open("web/bitnamiRemove.txt", "w") as fs:
+            fs.write("import os\n")            
+            for file in self.removeFiles:
+                #qpfile = os.path.join(facts.AWSIMG, file)
+                fs.write(f"os.remove({file})\n")
+        fs.close()
+
     
     def prepareAllFileRemoval(self):
         self.writeDropboxRemoveFiles()
         self.writeNativeRemoveFiles()
         self.writeBitnamiRemoveFiles()
-
+    def runner(self):
+        self.doOrDontRemove(facts.IMAGES)
 
 def main():
     rf = RemoveFiles(facts.lazyStates)
-    rf.doOrDontRemove(facts.DBIMG)
+    rf.runner()
+##    rf.doOrDontRemove(facts.IMAGES)
     
 if __name__ == "__main__":
     main()
