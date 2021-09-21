6baaaf78fed44c7d0817c5dfec5fa7eaceec2858
jim traugott <jtraug@gmail.com>
2021-09-21 16:36:43 -0700

    Initial commit

---
+++ nyt/build/activity.py
@@ -0,0 +1,39 @@
+''' 
+activity.py-
+List all python pgms and the associated state and county
+'''
+
+countpy = 'counties,py'
+statespy = 'states.py'
+lastday = 'lastday.py'
+
+oregon = 'Oregon'
+wash = 'Washington'
+calif = 'California'
+idaho = 'Idaho'
+socaro = 'South Carolina'
+texas = 'Texas'
+nj = 'New Jersey'
+
+
+pyStateAndCounty = [
+    [countpy, oregon, 'Curry'],
+    [countpy, oregon, 'Josephine'],
+    [countpy, oregon, 'Douglas'],
+    [countpy, texas, 'Harris'],
+    [countpy, socaro, 'Charleston'],
+    [countpy, wash, 'Snohomish'],
+    [countpy, wash, 'Spokane'],
+    [countpy, calif, 'Los Angeles'],
+    [statespy,  'Washington'],
+    [statespy,  calif],
+    [statespy,  oregon],
+    [statespy,  idaho],
+    [statespy,  wash],
+    [lastday, oregon],
+    [lastday, nj],
+    [lastday, socaro],
+    [lastday, idaho],
+    [lastday, wash],
+    [lastday, calif],
+]
