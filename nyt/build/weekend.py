2ff61acbafc0e5858a194017429bafcc0865e6c8
jim traugott <jtraug@gmail.com>
2021-09-21 16:36:20 -0700

    Snapshot

--- nyt/build/weekend.py
+++ nyt/build/weekend.py
@@ -6,7 +6,8 @@ make graphs on weekends.
 import timetools as tt #Uncover weekends
 import os
 import sys
-import facts
+import facts 
+import activity
 
 
 
@@ -16,7 +17,7 @@ import facts
 
 class RemoveFiles:
     def __init__(self, lazyStates):
-        self.removeFiles = []
+        self.weekendFiles = []
         self.empty = []
         self.lazyStates = lazyStates
         if self.is_weekend():
@@ -29,11 +30,11 @@ class RemoveFiles:
 
     def remove_not_weekend(self, pathname):
         for f in os.listdir(pathname):
-            self.removeFiles.append(f)
+            self.weekendFiles.append(f)
         self.prepareAllFileRemoval()
 
     def doOrDontRemove(self,  pathname):
-        ''' filled in later (init) to be one of the weekend funcs
+        ''' filled in later (by init) to be one of the weekend funcs
         '''
         ## assert False
         pass
@@ -53,13 +54,13 @@ class RemoveFiles:
                 #print(f"keep {f}")
             else:
                 # print(f"remove {f}")
-                self.removeFiles.append(f)
+                self.weekendFiles.append(f)
         self.prepareAllFileRemoval()
 
     def writeNativeRemoveFiles(self):
         with  open("nativeRemove.txt", "w") as fs:
             fs.write("import os\n")
-            for file in self.removeFiles:
+            for file in self.weekendFiles:
                 pfile = os.path.join(facts.IMAGES, file)
                 fs.write(f"os.remove({pfile})\n")
         fs.close()
@@ -67,7 +68,7 @@ class RemoveFiles:
     def  writeDropboxRemoveFiles(self):
         with  open("dboxRemove.txt", "w") as fs:
             fs.write("import os\n")
-            for file in self.removeFiles:
+            for file in self.weekendFiles:
                 pfile = os.path.join(facts.DBIMG, file)
                 fs.write(f"os.remove({pfile})\n")
         fs.close()
@@ -76,20 +77,27 @@ class RemoveFiles:
         
 
     def writeBitnamiRemoveFiles(self):
-        with  open("web/bitnamiRemove.txt", "w") as fs:
+        with  open("../web/bitnamiRemove.txt", "w") as fs:
             fs.write("import os\n")            
-            for file in self.removeFiles:
+            for file in self.weekendFiles:
                 #qpfile = os.path.join(facts.AWSIMG, file)
                 fs.write(f"os.remove({file})\n")
         fs.close()
+    def writeNativeBuildFiles():
+        pass
 
+    def writeDropboxBuildFiles():
+        pass
+    
+    def writeBitnamiBuildFiles():
+        pass
     
     def prepareAllFileRemoval(self):
         self.writeDropboxRemoveFiles()
         self.writeNativeRemoveFiles()
         self.writeBitnamiRemoveFiles()
 
-        def runner(self):
+    def runner(self):
         self.doOrDontRemove(facts.IMAGES)
 
 def main():
