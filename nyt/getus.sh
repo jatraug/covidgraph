80a99e45bc81b4481d4d91b6cb955658be3de827
jim traugott <jtraug@gmail.com>
2021-10-01 11:37:32 -0700

    added a tail

--- nyt/getus.sh
+++ nyt/getus.sh
@@ -1,3 +1,5 @@
 ## -*-sh-*-
 
-    curl -o datasets/us.csv https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv
+curl -o datasets/us.csv https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv
+
+tail datasets/us.csv
