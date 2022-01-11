4232279c02faa98eb780b98310c8e142e9fa9bb4
jim traugott <jtraug@gmail.com>
2022-01-10 16:14:57 -0800

    Snapshot

--- nyt/testbuild.py
+++ nyt/testbuild.py
@@ -3,14 +3,14 @@ import os
 
 
 # func to make a python buid order
-
+ 
 def printAndBuildCounty(**kwargs):
     pyfile = 'counties.py'
     state = kwargs['state'].title()
     cnty = kwargs['county'].title()
-    cmd = f"python {pyfile} -n -s {state} -c {cnty}"
+    cmd = f"python {pyfile} -n -s \'{state}\' -c \'{cnty}\'"
     print(cmd)
-    print(f"os.system(python {pyfile} -n -s \'{state}\' -c \'{cnty}\')")
+    #print(f"os.system(python {pyfile} -n -s \'{state}\' -c \'{cnty}\')")
     os.system(cmd)
 
     
@@ -18,7 +18,7 @@ def printAndBuildCounty(**kwargs):
 def printAndBuildState(**kwargs):
     pyfile = 'states.py'
     state = kwargs['state'].title()
-    cmd = f"python {pyfile} -n -s {state}"
+    cmd = f"python {pyfile} -n -s \'{state}\'"
     print(cmd)
     os.system(cmd)
 
@@ -36,8 +36,33 @@ def mkPyBuildOrder(**kwargs):
 #mkPyBuildOrder(file='counties',state='Washington', county='Snohomish')
 buildData = [
     {'file': 'counties', 'state': 'Washington', 'county': 'Snohomish'},
-    {'file': 'states', 'state': 'Mississippi'}
+    {'file': 'counties', 'state': 'Washingon', 'county': 'Spokane'},
+    {'file': 'counties', 'state': 'Oregon', 'county': 'Douglas'},
+    {'file': 'counties', 'state': 'Oregon', 'county': 'Curry'},
+    {'file': 'counties', 'state': 'Oregon', 'county': 'Josephine'},
+    {'file': 'counties', 'state': 'Texas', 'county': 'Harris'},
+    {'file': 'counties', 'state': 'California', 'county': 'Los Angeles'},
+    {'file': 'counties', 'state': 'South Carolina', 'county': 'Charleston'},
+
+    {'file': 'states', 'state': 'Mississippi'},
+    {'file': 'states', 'state': 'california'},
+    {'file': 'states', 'state': 'oregon'},
+    {'file': 'states', 'state': 'idaho'},
+    {'file': 'states', 'state': 'florida'},
+    {'file': 'states', 'state': 'new york'},
+    {'file': 'states', 'state': 'washington'},
+    {'file': 'states', 'state': 'ohio'},
+    {'file': 'states', 'state': 'missouri'},
+    {'file': 'states', 'state': 'minnesota'},
+    {'file': 'states', 'state': 'wisconsin'},
+    {'file': 'states', 'state': 'new jersey'},
+    {'file': 'states', 'state': 'south carolina'},
+    {'file': 'states', 'state': 'arizona'},
+    {'file': 'states', 'state': 'alaska'},
+
  ]
+
+
 
 def doBuild():
     for line in buildData:
