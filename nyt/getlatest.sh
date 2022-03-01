65d15c3f71b85c9808dcda093f074fa19a48c594
jim traugott <jtraug@gmail.com>
2022-03-01 13:01:03 -0800

    Added two mkdir cmds

--- nyt/getlatest.sh
+++ nyt/getlatest.sh
@@ -1,5 +1,8 @@
 ## -*-sh-*-
 
+mkdir -p datasets
+mkdir -p images
+
     curl -o datasets/us-states.csv https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv
 
 curl -o datasets/us-counties.csv https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv
