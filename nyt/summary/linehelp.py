745d279dd6d1e1729f516695f812f90badc3a7dc
jim traugott <jtraug@gmail.com>
2022-04-06 11:22:47 -0700

    initial checkin

---
+++ nyt/summary/linehelp.py
@@ -0,0 +1,83 @@
+import re
+
+
+
+class Changeline():
+    def __init__(self):
+        self.lDict = {} 
+        self.linemax = 0
+        self.linedictarr = []
+        self.linedict = {}
+        self.line0 = "\nAverage Cases/Deaths"
+        self.line1 = ''
+        self.cases = ''
+        self.deaths = ''
+    
+    def maxChars(self, lines):
+        newlist = []
+        for line in lines:
+            newlist.append(len(line))
+        self.linemax = max(newlist)
+        ##return max(newlist)
+    
+        
+    def changeLine(self, line):
+        print(line)
+        ##exp = re.compile('(.* on /n{4}-\n{2}-\n{2}\:)\s+(\n+)') ## ([{Cases|Deaths}])
+        exp = re.compile('A.*\s(Cases|Deaths)(.* on\s+\d{4}-\d{2}-\d{2}\:\s+)(\d+)') ## ([{Cases|Deaths}])
+        matchObj = re.search(exp, line)
+        spc = '          '
+        if (matchObj):
+            #[print(m) for m in matchObj]
+            #print(matchObj.groups())
+            grp = matchObj.groups()
+            #print(f'0: {grp[0]}')
+            self.line1 = grp[1]
+            if grp[0]== 'Deaths':
+                self.deaths = grp[2]
+            elif grp[0] == 'Cases':
+                self.cases = grp[2]
+                self.outputLine()
+                self.emptyGroups()
+            else:
+                assert False
+                
+    def emptyGroups(self):
+        self.cases = ''
+        self.deaths = ''
+        self.line1  = ''
+        
+    def outputLine(self):
+        line = f'{self.line0} {self.line1}:  {self.cases}   {self.deaths}\n'
+        print(line)
+                    
+    
+    def runner(self):
+        """
+        First read the file
+        """
+        with open('/Users/jimt/work/covid/nyt/html/Covid_Status_2022-03-30.txt', 'r') as fs:
+            self.lines = fs.read().split('\n')
+
+        mkTopLine(79)
+            
+        for line in self.lines:
+            self.changeLine(line)
+
+###=============== End of Ckass =======================
+
+def strOfSpaces(count, ch = ' '):
+    
+    return (ch * count)
+
+def mkTopLine(len):
+    topline = strOfSpaces(len)
+    print(f'{topline}Cases  Deaaths')
+
+def main():
+    cl = Changeline()
+    cl.runner()
+
+
+if __name__ == '__main__':
+    main()
