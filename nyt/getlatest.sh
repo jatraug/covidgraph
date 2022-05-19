16463f78bd819cf31bd1945bb9d27739f86d1f2b
jim traugott <jtraug@gmail.com>
2022-05-19 12:25:02 -0700

    Needed changew for adding us-counties-2022.csv

--- nyt/getlatest.sh
+++ nyt/getlatest.sh
@@ -5,9 +5,11 @@ mkdir -p images
 
     curl -o datasets/us-states.csv https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv
 
-curl -o datasets/us-counties.csv https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv
+    curl -o datasets/us-counties.csv https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv
+    curl -o datasets/us-counties-2022.csv https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv
 
 curl -o datasets/us.csv https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv
 
 curl -o datasets/us-counties-2022.csv https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties-2022.csv
+curl -o datasets/us-counties-2022.csv https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties-recent.csv
 
