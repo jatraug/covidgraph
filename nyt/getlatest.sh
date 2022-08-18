6decff53f16c0d8f0d062ca1c75dbf1bf81dba51
jim traugott <jtraug@gmail.com>
2022-08-17 17:14:03 -0700

    Corrected dnld of us-counties-2022.csv

--- nyt/getlatest.sh
+++ nyt/getlatest.sh
@@ -6,7 +6,8 @@ mkdir -p images
     curl -o datasets/us-states.csv https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv
 
     curl -o datasets/us-counties.csv https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv
-    curl -o datasets/us-counties-2022.csv https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv
+    
+##    curl -o datasets/us-counties-2022.csv https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv
 
 curl -o datasets/us.csv https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv
 
